203a47c8de52ffddda776a6ede24785f935317b6
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-11-03 21:45:42 +0000

    #4 Add __get_pattern function to get the entanglement unitary

--- src/layers/entanglement_unitary_layer.py
+++ src/layers/entanglement_unitary_layer.py
@@ -1,3 +1,5 @@
+import qiskit
+
 from src.layers.base_learnable_layer import BaseLearnableLayer
 
 
@@ -6,3 +8,17 @@ class EntanglementUnitaryLayer(BaseLearnableLayer):
 
     def __init__(self, param_prefix: str):
         super().__init__(param_prefix=param_prefix)
+
+    def __get_pattern(
+        self, params: qiskit.circuit.ParameterVector
+    ) -> qiskit.QuantumCircuit:
+        """Return the entanglement unitary layer pattern.
+
+        :param qiskit.circuit.ParameterVector params: parameter vector
+        :return qiskit.QuantumCircuit: entanglement unitary layer pattern
+        """
+        pattern = qiskit.QuantumCircuit(2)
+        pattern.cry(params[0], 0, 1)
+        pattern.crz(params[1], 0, 1)
+
+        return pattern
