b505932776e37c81ba1bdf1e3f41c50fc88f7321
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-11-03 20:53:03 +0000

    #2 Add get_circuit functino to get the single qubit unitary layer circuit

--- src/layers/single_qubit_unitary_layer.py
+++ src/layers/single_qubit_unitary_layer.py
@@ -22,3 +22,34 @@ class SingleQubitUnitaryLayer(BaseLearnableLayer):
         pattern.rz(params[1], 0)
 
         return pattern
+
+    def get_circuit(
+        self, num_qubits: int, applied_qubits: list[int]
+    ) -> qiskit.QuantumCircuit:
+        """Get the single qubit unitary layer circuit.
+
+        :param int num_qubits: number of qubits
+        :param list[int] applied_qubits: list of qubits to which single qubit unitary is applied
+        :return qiskit.QuantumCircuit: single qubit unitary layer circuit
+        """
+        # Get parameters.
+        num_params = len(applied_qubits) * 2
+        params = qiskit.circuit.ParameterVector(self.param_prefix, length=num_params)
+
+        # Make a quantum circuit having single qubit unitary at the specified qubits.
+        circuit = qiskit.QuantumCircuit(num_qubits, name="Single Qubit Unitary Layer")
+        for index, applied_qubit in enumerate(applied_qubits):
+            param_start_index = index * 2
+            circuit.compose(
+                self.__get_pattern(
+                    params=params[param_start_index : 2 + param_start_index],
+                ),
+                applied_qubit,
+                inplace=True,
+            )
+
+        circuit_inst = circuit.to_instruction()
+        circuit = qiskit.QuantumCircuit(num_qubits)
+        circuit.append(circuit_inst, list(range(num_qubits)))
+
+        return circuit
