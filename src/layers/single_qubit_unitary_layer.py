2502ec0449b234d85718e8583f695b3bb1fad2f7
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-11-03 19:46:35 +0000

    #2 Add __get_pattern function to make the single qubit unitary

--- src/layers/single_qubit_unitary_layer.py
+++ src/layers/single_qubit_unitary_layer.py
@@ -1,3 +1,5 @@
+import qiskit
+
 from src.layers.base_learnable_layer import BaseLearnableLayer
 
 
@@ -6,3 +8,15 @@ class SingleQubitUnitaryLayer(BaseLearnableLayer):
 
     def __init__(self, param_prefix: str):
         super().__init__(param_prefix=param_prefix)
+
+    def __get_pattern(
+        self, params: qiskit.circuit.ParameterVector
+    ) -> qiskit.QuantumCircuit:
+        """Return the single qubit unitary layer pattern.
+
+        :param qiskit.circuit.ParameterVector params: parameter vector
+        :return qiskit.QuantumCircuit: single qubit unitary layer pattern
+        """
+        pattern = qiskit.QuantumCircuit(1)
+        pattern.ry(params[0], 0)
+        pattern.rz(params[1], 0)
