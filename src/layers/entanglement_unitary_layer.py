44011261fadbae85472241fcc5aeeee9d5895ca0
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-11-03 21:50:49 +0000

    #4 Create get_circuit function to get the entanglement unitary layer

--- src/layers/entanglement_unitary_layer.py
+++ src/layers/entanglement_unitary_layer.py
@@ -22,3 +22,34 @@ class EntanglementUnitaryLayer(BaseLearnableLayer):
         pattern.crz(params[1], 0, 1)
 
         return pattern
+
+    def get_circuit(
+        self, num_qubits: int, applied_qubit_pairs: list[tuple[int, int]]
+    ) -> qiskit.QuantumCircuit:
+        """Get the entanglement unitary layer circuit.
+
+        :param int num_qubits: number of qubits
+        :param list[int] applied_qubit_pairs: list of qubit pairs, (control qubit, target qubit), to which entanglement unitary is applied
+        :return qiskit.QuantumCircuit: entanglement unitary layer circuit
+        """
+        # Get parameters.
+        num_params = len(applied_qubit_pairs) * 2
+        params = qiskit.circuit.ParameterVector(self.param_prefix, length=num_params)
+
+        # Make a quantum circuit having the entanglement unitary at the specified qubit pairs.
+        circuit = qiskit.QuantumCircuit(num_qubits, name="Entanglement Unitary Layer")
+        for index, applied_qubit_pair in enumerate(applied_qubit_pairs):
+            param_start_index = index * 2
+            circuit.compose(
+                self.__get_pattern(
+                    params=params[param_start_index : 2 + param_start_index],
+                ),
+                applied_qubit_pair,
+                inplace=True,
+            )
+
+        circuit_inst = circuit.to_instruction()
+        circuit = qiskit.QuantumCircuit(num_qubits)
+        circuit.append(circuit_inst, list(range(num_qubits)))
+
+        return circuit
