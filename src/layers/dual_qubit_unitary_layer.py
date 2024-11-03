aab4f68342aca7ceaa8467c1a72b98a1199dbaf4
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-11-03 21:27:40 +0000

    #3 Add __get_pattern function to get the dual qubit unitary

--- src/layers/dual_qubit_unitary_layer.py
+++ src/layers/dual_qubit_unitary_layer.py
@@ -1,3 +1,5 @@
+import qiskit
+
 from src.layers.base_learnable_layer import BaseLearnableLayer
 
 
@@ -6,3 +8,17 @@ class DualQubitUnitaryLayer(BaseLearnableLayer):
 
     def __init__(self, param_prefix: str):
         super().__init__(param_prefix=param_prefix)
+
+    def __get_pattern(
+        self, params: qiskit.circuit.ParameterVector
+    ) -> qiskit.QuantumCircuit:
+        """Return the dual qubit unitary layer pattern.
+
+        :param qiskit.circuit.ParameterVector params: parameter vector
+        :return qiskit.QuantumCircuit: dual qubit unitary layer pattern
+        """
+        pattern = qiskit.QuantumCircuit(2)
+        pattern.rry(params[0], 0, 1)
+        pattern.rrz(params[1], 0, 1)
+
+        return pattern
