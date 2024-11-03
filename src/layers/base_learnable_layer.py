1bf5ff3307633a1f7346bbd7228bd670f149e060
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-11-03 19:35:24 +0000

    #5 Adapt the chenge of BaseLayer

--- src/layers/base_learnable_layer.py
+++ src/layers/base_learnable_layer.py
@@ -6,6 +6,6 @@ from src.bases.base_layer import BaseLayer
 class BaseLearnableLayer(ABC, BaseLayer):
     """BaseLearnableLayer abstract class."""
 
-    def __init__(self, qubits: list[int], param_prefix: str):
-        super().__init__(qubits=qubits)
+    def __init__(self, param_prefix: str):
+        super().__init__()
         self.param_prefix = param_prefix
