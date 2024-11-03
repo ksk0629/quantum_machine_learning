b5afa6e5e22feaae5a4d2601ccc9f32ee64bdb4e
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-11-03 19:54:20 +0000

    #5 Fix the bug; TypeError: Cannot create a consistent method resolution order (MRO) for bases ABC, BaseLayer

--- src/layers/base_learnable_layer.py
+++ src/layers/base_learnable_layer.py
@@ -3,7 +3,7 @@ from abc import ABC
 from src.layers.base_layer import BaseLayer
 
 
-class BaseLearnableLayer(ABC, BaseLayer):
+class BaseLearnableLayer(BaseLayer, ABC):
     """BaseLearnableLayer abstract class."""
 
     def __init__(self, param_prefix: str):
