deade53a2e2248501ff4aad8cec6b31d4390e4bd
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-11-17 23:48:51 +0000

    Add positive-negative numbers dataset as an artificial dataset

--- src/dataset.py
+++ src/dataset.py
@@ -13,6 +13,8 @@ def get_dataset(name: str) -> tuple[np.ndarray, list[str]]:
             return get_iris()
         case "wine":
             return get_wine()
+        case "pos_neg":
+            return get_pos_neg()
 
 
 def get_iris() -> tuple[np.ndarray, list[str]]:
@@ -37,4 +39,25 @@ def get_wine() -> tuple[np.ndarray, list[str]]:
     wine_data = datasets.load_wine(return_X_y=True)
     data, labels = wine_data
     labels = [f"{label}" for label in labels]
+    return data, labels
+
+
+def get_pos_neg(
+    dimension: int = 4, num_data: int = 100, highest=1
+) -> tuple[np.ndarray, list[str]]:
+    """Get a positive-negative numbers dataset,
+    which is randomly generated.
+    The label of each data is "positive" if the summation of the data is non-negative.
+    Otherwise, it is "negative".
+
+    :param int dimension: dimension of data, defaults to 4
+    :param int num_data: number of data, defaults to 100
+    :param int highest: highest number, defaults to 1
+    :return tuple[np.ndarray, list[str]]: positive-negative numbers dataset
+    """
+    data = np.random.rand(num_data, dimension) * highest
+    labels = ["positive" if np.sum(_d) >= 0 else "negative" for _d in data]
+
+    assert len(data) == len(labels)
+
     return data, labels
