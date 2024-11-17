0c17e8719f7b74366ef29f064247d8fe68c6e704
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-11-17 23:49:10 +0000

    Add SwapTestLayer class and its test case

---
+++ tests/test_layers/test_swap_test_layer.py
@@ -0,0 +1,23 @@
+import pytest
+
+from src.layers.swap_test_layer import SwapTestLayer
+
+
+class TestSwapTestLayer:
+    @classmethod
+    def setup_class(cls):
+        cls.control_qubit = 0
+        cls.qubit_pairs = [(1, 4), (2, 5), (3, 6)]
+        cls.swap_test_layer = SwapTestLayer(
+            control_qubit=cls.control_qubit,
+            qubit_pairs=cls.qubit_pairs,
+        )
+
+    def test_init(self):
+        """Normal test
+        Check if the class that was prepared in setup_class has
+        - the same control_qubit as self.control_qubit.
+        - the same qubit_pairs as self.qubit_pairs.
+        """
+        assert self.swap_test_layer.control_qubit == self.control_qubit
+        assert self.swap_test_layer.qubit_pairs == self.qubit_pairs
