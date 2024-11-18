f20215073a32a0479479fa7069f67ab0e2dd38f0
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-11-18 01:28:33 +0000

    Add large and small dataset

--- src/dataset.py
+++ src/dataset.py
@@ -15,6 +15,8 @@ def get_dataset(name: str) -> tuple[np.ndarray, list[str]]:
             return get_wine()
         case "pos_neg":
             return get_pos_neg()
+        case "get_large_small":
+            return get_large_small()
 
 
 def get_iris() -> tuple[np.ndarray, list[str]]:
@@ -45,8 +47,7 @@ def get_wine() -> tuple[np.ndarray, list[str]]:
 def get_pos_neg(
     dimension: int = 4, num_data: int = 100, highest=1
 ) -> tuple[np.ndarray, list[str]]:
-    """Get a positive-negative numbers dataset,
-    which is randomly generated.
+    """Get a positive-negative numbers dataset, which is randomly generated.
     The label of each data is "positive" if the summation of the data is non-negative.
     Otherwise, it is "negative".
 
@@ -58,6 +59,34 @@ def get_pos_neg(
     data = np.random.rand(num_data, dimension) * highest
     data -= highest / 2
     labels = ["positive" if np.sum(_d) >= 0 else "negative" for _d in data]
+
+    assert len(data) == len(labels)
+
+    return data, labels
+
+
+def get_large_small(
+    dimension: int = 4, num_data: int = 100, highest=1
+) -> tuple[np.ndarray, list[str]]:
+    """Get a large-small numbers dataset, which is randomly generated.
+    The label of each data is "large" if all the entries of the data are larger than or equal to
+    the half of the given highest. Otherwise, it is "small".
+    Note that, in the current implementation, if it is small, then all the entries are smaller
+    than the half of the given highest.
+
+    :param int dimension: dimension of data, defaults to 4
+    :param int num_data: number of data, defaults to 100
+    :param int highest: highest number, defaults to 1
+    :return tuple[np.ndarray, list[str]]: large-small numbers dataset
+    """
+    small_data = np.random.rand(num_data // 2, dimension) * (highest / 2)
+    large_data = np.random.rand(num_data // 2, dimension) * (highest / 2)
+    large_data += highest / 2
+    data = np.concatenate((small_data, large_data))
+
+    small_labels = ["small"] * len(small_data)
+    large_labels = ["large"] * len(large_data)
+    labels = small_labels + large_labels
 
     assert len(data) == len(labels)
 
