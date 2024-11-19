1aa96871aa26a7a29aa08cde0a0ac8ab3773b1d3
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-11-19 18:49:17 +0000

    #14 Create an initial QuanvLayer class

---
+++ src/quanv_nn/quanv_layer.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+from src.layers.random_layer import RandomLayer
+
+
+class QuanvLayer:
+    """Quanvolutional layer class."""
+
+    def __init__(self, kernel_size: tuple[int, int], num_filters: int):
+        pass
+
+    def __call__(self, x: np.ndarray):
+        pass
