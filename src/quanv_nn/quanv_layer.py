0290903f7a32eac58704d940b2fee0db3d1e4d80
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-11-19 19:26:44 +0000

    #14 Implement __init__

--- src/quanv_nn/quanv_layer.py
+++ src/quanv_nn/quanv_layer.py
@@ -1,5 +1,8 @@
 import numpy as np
+import qiskit
+from qiskit import qpy, primitives
 
+from src.encoders.x_encoder import XEncoder
 from src.layers.random_layer import RandomLayer
 
 
@@ -7,7 +10,34 @@ class QuanvLayer:
     """Quanvolutional layer class."""
 
     def __init__(self, kernel_size: tuple[int, int], num_filters: int):
-        pass
+        """Initialise the quanvolutional layer.
 
-    def __call__(self, x: np.ndarray):
+        :param tuple[int, int] kernel_size: kernel size
+        :param int num_filters: number of filiters
+        """
+        self.kernel_size = kernel_size
+        self.num_filters = num_filters
+
+        num_qubits = self.kernel_size[0] * self.kernel_size[1]
+        circuit = qiskit.QuantumCircuit(num_qubits, name="QuanvFilter")
+        circuit.compose(
+            XEncoder(num_qubits=num_qubits)(), range(num_qubits), inplace=True
+        )
+        circuit.barrier()
+
+        self.filters = [
+            circuit.compose(
+                RandomLayer(num_qubits=num_qubits)(), range(num_qubits), inplace=False
+            )
+            for _ in range(self.num_filters)
+        ]
+
+    def __call__(
+        self,
+        data: np.ndarray,
+        sampler: (
+            primitives.BaseSamplerV1 | primitives.BaseSamplerV2
+        ) = primitives.StatevectorSampler(seed=901),
+        shots: int = 8096,
+    ):
         pass
