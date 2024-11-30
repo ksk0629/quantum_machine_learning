bac88e6468393905b0e72acd89c0f5c9093d28e2
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2024-11-30 22:39:56 +0000

    Add calc_2d_output_shape function and its test case

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -265,3 +265,19 @@ class TestUtils:
         )
         assert encoded_data.sum() == threshold
         assert data.shape == encoded_data.shape
+
+    def test_calc_2d_output_shape(self):
+        """Normal test;
+        run calc_2d_output_shape.
+
+        Check if the returned shape is appropriate.
+        """
+        height = 50
+        width = 25
+        kernel_size = (5, 5)
+        (out_height, out_width) = utils.calc_2d_output_shape(
+            height=height, width=width, kernel_size=kernel_size
+        )
+
+        assert out_height == 46
+        assert out_width == 21
