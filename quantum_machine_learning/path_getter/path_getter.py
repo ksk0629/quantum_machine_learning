e6ffe01b146e2cb878921f33125b06637504c208
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2025-03-22 04:28:21 -0400

    #24 Create PathGetter class

---
+++ quantum_machine_learning/path_getter/path_getter.py
@@ -0,0 +1,36 @@
+from abc import ABC
+import os
+
+
+class PathGetter(ABC):
+    """A base class to get fixed paths."""
+
+    def __init__(
+        self, dir_path: str, prefix: None | str = None, postfix: None | str = None
+    ):
+        """Initialise this path getter.
+
+        :param str dir_path: _description_
+        :param None | str prefix: _description_, defaults to None
+        :param None | str postfix: _description_, defaults to None
+        """
+        self._dir_path = dir_path
+        self._prefix = prefix
+        self._postfix = postfix
+
+    def _get_path(self, filename: str, extension: str) -> str:
+        """Get the path with the prefix and postfix.
+
+        :param str filename: filename
+        :param str extension: extension without dot
+        :return str: path to the file
+        """
+        # FOR DEVELOPER: The extension must not start with ".".
+        assert extension[0] != "."
+
+        path = filename
+        path = path if self._prefix is None else f"{self._prefix}_{path}"
+        path = path if self._postfix is None else f"{path}_{self._postfix}"
+        path = f"{path}.{extension}"
+
+        return os.path.join(self._dir_path, path)
