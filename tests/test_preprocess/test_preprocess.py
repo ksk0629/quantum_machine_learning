5bd0196e24fa285e041b4bcd9bb0019fa25068cc
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2025-03-22 02:32:08 -0400

    #24 Add a test for evenise_data_dimension

--- tests/test_preprocess/test_preprocess.py
+++ tests/test_preprocess/test_preprocess.py
@@ -29,3 +29,35 @@ class TestUtils:
         normalised_vectors = np.array(normalised_vectors)
 
         assert np.allclose(result, normalised_vectors)
+
+    @pytest.mark.preprocessor
+    @pytest.mark.parametrize(
+        "data_and_filling_value",
+        [
+            ([[2, 3], [1, 1]], 0),
+            ([[1, 1, 1]], 1),
+            ([[1, 1], [1, 1]], 0),
+            ([[1, 1, 1], [1, 1, 1]], 0),
+        ],
+    )
+    def test_evenise_data_dimension(self, data_and_filling_value):
+        """Normal test;
+        Run evenise_data_dimension.
+
+        Check if the dimension of each datum is even.
+        """
+        (data, filling_value) = data_and_filling_value
+        data = np.array(data)
+        result = Preprocessor.evenise_data_dimension(
+            data=data, filling_value=filling_value
+        )
+
+        if data.shape[1] % 2 == 0:
+            # If the original dimension is even, then nothing must have happened.
+            assert np.allclose(result, data)
+        else:
+            # If the original dimension is odd, then the first number of elements must be the same as the original data.
+            assert np.allclose(result[:, :-1], data)
+            # Also, the last elements must be the same as the filling value.
+            fillings = np.zeros((result.shape[0],)) + filling_value
+            assert np.allclose(result[:, -1], fillings)
