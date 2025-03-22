2de810dcdf53478635950934fd6810f77d7d0b13
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2025-03-22 12:01:02 -0400

    #24 Move and modify tests for calculating fidelity

---
+++ tests/test_postprocessor/test_postprocessor.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pytest
+
+from quantum_machine_learning.postprocessor.postprocessor import Postprocessor
+
+
+class TestPostprocessor:
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    @pytest.mark.postprocessor
+    @pytest.mark.parametrize(
+        "result",
+        [
+            {"0": 1, "1": 1, "2": 1},
+            {},
+            {"0": 1, "-1": 1},
+            {"1": 1, "3": 1},
+            {"-2": 1},
+        ],
+    )
+    def test_calculate_fidelity_from_swap_test_with_invalid_arg(self, result):
+        """Abnormal test;
+        Run calculate_fidelity_from_swap_test with an invalid argument.
+
+        Check if ValueError happens.
+        """
+        with pytest.raises(ValueError):
+            Postprocessor.calculate_fidelity_from_swap_test(result)
+
+    @pytest.mark.postprocessor
+    def test_calculate_fidelity_from_swap_test_with_zero(self):
+        """Normal test;
+        Run calculate_fidelity_from_swap_test with a result having only zero observed.
+
+        Check if
+        - the return value is 1.
+        """
+        result = {"0": 8192}
+        fidelity = Postprocessor.calculate_fidelity_from_swap_test(result)
+        assert fidelity == 1
+
+    @pytest.mark.postprocessor
+    def test_calculate_fidelity_from_swap_test_with_zero(self):
+        """Normal test;
+        Run calculate_fidelity_from_swap_test with a result having only one observed.
+
+        Check if
+        - the return value is 0.
+        """
+        result = {"1": 8192}
+        fidelity = Postprocessor.calculate_fidelity_from_swap_test(result)
+        assert fidelity == 0
+
+    @pytest.mark.postprocessor
+    def test_calculate_fidelity_from_swap_test_with_half_zero_and_half_one(self):
+        """Normal test;
+        Run calculate_fidelity_from_swap_test with a result meaning the fidelity is 1/2.
+
+        Check if
+        - the return value is 1/2.
+        """
+        result = {"0": 3, "1": 1}
+        fidelity = Postprocessor.calculate_fidelity_from_swap_test(result)
+        assert fidelity == 1 / 2
