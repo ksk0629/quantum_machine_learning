c0c4424d30ffb3b101d5395e64f5eea3f47d4977
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2025-03-24 01:51:37 -0400

    #25 Update YZEncoder and its tests to be along with the new BaseEncoder

--- quantum_machine_learning/encoders/yz_encoder.py
+++ quantum_machine_learning/encoders/yz_encoder.py
@@ -1,4 +1,7 @@
+from typing import Callable
+
 import qiskit
+import qiskit.circuit
 
 from quantum_machine_learning.encoders.base_encoder import BaseEncoder
 
@@ -6,31 +9,87 @@ from quantum_machine_learning.encoders.base_encoder import BaseEncoder
 class YZEncoder(BaseEncoder):
     """YZEncoder class, which is used in https://arxiv.org/pdf/2103.11307."""
 
-    def __init__(self, num_qubits: int):
+    def __init__(
+        self,
+        data_dimension: int,
+        name: str = "yz_encoder",
+        transformer: Callable[[list[float]], list[float]] | None = None,
+    ):
         """Initialise this encoder.
 
-        :param int num_qubits: number of qubits.
+        :param int data_dimension: the dimension of data
+        :param str | None name: the name of this encoder, defaults to None
+        :param Callable[[list[float]], list[float]] | None transformer: the data transformer, defaults to None
         """
-        self.num_qubits = num_qubits
+        self._y_parameters = None
+        self._z_parameters = None
+
+        super().__init__(
+            data_dimension=data_dimension, name=name, transformer=transformer
+        )
 
     @property
-    def num_params(self) -> int:
-        return self.num_qubits * 2
+    def num_encoding_qubits(self) -> int:
+        """Return the number of qubits to be encoded.
 
-    def get_circuit(self) -> qiskit.QuantumCircuit:
-        # Get parameters.
-        params = qiskit.circuit.ParameterVector("x", length=self.num_params)
+        :return int: the number of encoding qubits
+        """
+        if self.data_dimension % 2 == 0:  # Even number
+            return int(self.data_dimension // 2)
+        else:  # Odd number
+            return int((self.data_dimension + 1) // 2)
 
-        circuit = qiskit.QuantumCircuit(self.num_qubits, name="YZEncoder")
-        for index_qubit in range(self.num_qubits):
-            index_first_param = index_qubit * 2
-            circuit.ry(params[index_first_param], index_qubit)
+    @property
+    def y_parameters(self) -> qiskit.circuit.ParameterVector:
+        """Return the parameter vector for the Y-rotation of this circuit.
 
-            index_second_param = index_first_param + 1
-            circuit.rz(params[index_second_param], index_qubit)
+        :return qiskit.circuit.ParameterVecotr: the Y-rotation parameter vector
+        """
+        return self._y_parameters
 
-        circuit_inst = circuit.to_instruction()
-        circuit = qiskit.QuantumCircuit(self.num_qubits)
-        circuit.append(circuit_inst, list(range(self.num_qubits)))
+    @property
+    def z_parameters(self) -> qiskit.circuit.ParameterVector:
+        """Return the parameter vector for the Z-rotation of this circuit.
 
-        return circuit
+        :return qiskit.circuit.ParameterVecotr: the Z-rotation parameter vector
+        """
+        return self._z_parameters
+
+    def _check_configuration(self, raise_on_failure=True) -> bool:
+        """Check if the current configuration is valid.
+
+        :param bool raise_on_failure: if raise an error or not, defaults to True
+        :return bool: if the configuration is valid
+        """
+        valid = True
+
+        return valid
+
+    def _reset_register(self) -> None:
+        """Reset the register."""
+        qreg = qiskit.QuantumRegister(self.num_encoding_qubits)
+        self.qregs = [qreg]
+
+    def _reset_parameters(self) -> None:
+        """Reset the parameter vector."""
+        self._y_parameters = qiskit.circuit.ParameterVector(
+            "y", length=self.num_encoding_qubits
+        )
+        self._z_parameters = qiskit.circuit.ParameterVector(
+            "z", length=self.num_encoding_qubits
+        )
+        self._parameters = [self.y_parameters, self.z_parameters]
+
+    def _build(self) -> None:
+        """Build the circuit."""
+        super()._build()
+
+        # Make the quantum circuit.
+        circuit = qiskit.QuantumCircuit(*self.qregs)
+
+        # Add the encoding part: the rotation Y and Z.
+        for index in range(self.num_encoding_qubits):
+            circuit.ry(self.y_parameters[index], index)
+            circuit.rz(self.z_parameters[index], index)
+
+        self.append(circuit.to_gate(), self.qubits)
--- tests/test_encoders/test_yz_encoder.py
+++ tests/test_encoders/test_yz_encoder.py
@@ -1,4 +1,5 @@
 import pytest
+import qiskit
 
 from quantum_machine_learning.encoders.yz_encoder import YZEncoder
 
@@ -6,56 +7,55 @@ from quantum_machine_learning.encoders.yz_encoder import YZEncoder
 class TestYZEncoder:
     @classmethod
     def setup_class(cls):
-        cls.num_qubits = 4
-        cls.yz_encoder = YZEncoder(
-            num_qubits=cls.num_qubits,
-        )
+        pass
 
-    def test_init(self):
+    @pytest.mark.encoder
+    @pytest.mark.parametrize("data_dimension", [2, 3])
+    def test_init(self, data_dimension):
         """Normal test;
-        Check if the yz_encoder created in setup_class has
-        - num_qubits as same as self.num_qubits.
-        - num_params as double as self.num_qubits.
-        """
-        assert self.yz_encoder.num_qubits == self.num_qubits
-        assert self.yz_encoder.num_params == self.num_qubits * 2
-
-    @pytest.mark.parametrize("num_qubits", [2, 4, 5, 10])
-    def test_get_circuit(self, num_qubits):
-        """Normal test;
-        Run get_circuit with various num_qubits.
+        Create an YZEcnoder instance and chenge the data dimension.
 
         Check if
-        - the number of qubits of the circuit is the same as num_qubits.
-        - the number of paramters of the circuit is the double of num_qubits.
-        - each qubit has only two gates, ry and rz in this order.
+        - its num_encoding_qubits is
+            - the half of the given data_dimension if it is even.
+            - the half of the 1 + the given data_dimension if it is odd.
+        - its num_parameters is
+            - the same as the given data_dimension if it is even.
+            - the same as 1 + the given data_dimension if it is odd.
+        - the type of its parameters is list.
+        - the length of its parameters is 2.
+        - the type of the first element of its parameters is qiskit.circuit.ParameterVector.
+        - the type of the second element of its parameters is qiskit.circuit.ParameterVector.
+        - the length of the first element of its parameters is
+            - the half of the given data_dimension if it is even.
+            - the half of the 1 + the given data_dimension if it is odd.
+        - the above things are preserved with the new_data_dimension after substituting new_data_dimension.
         """
-        yz_encoder = YZEncoder(num_qubits=num_qubits)
-        circuit = yz_encoder().decompose()
+        yz_encoder = YZEncoder(data_dimension=data_dimension)
+        assert isinstance(yz_encoder.parameters, list)
+        assert len(yz_encoder.parameters) == 2
+        assert isinstance(yz_encoder.parameters[0], qiskit.circuit.ParameterVector)
+        assert isinstance(yz_encoder.parameters[1], qiskit.circuit.ParameterVector)
+        if data_dimension % 2 == 0:  # Even
+            assert yz_encoder.num_parameters == data_dimension
+            assert yz_encoder.num_encoding_qubits == data_dimension // 2
+            assert len(yz_encoder.parameters[0]) == data_dimension // 2
+        else:  # Odd
+            assert yz_encoder.num_parameters == data_dimension + 1
+            assert yz_encoder.num_encoding_qubits == (data_dimension + 1) // 2
+            assert len(yz_encoder.parameters[0]) == (data_dimension + 1) // 2
 
-        # Check the number of qubits.
-        assert circuit.num_qubits == num_qubits
-        # Check the number of parameters.
-        assert len(circuit.parameters) == num_qubits * 2
-
-        qubit_index = 0
-        gates = []
-        for index_inst, instruction in enumerate(circuit.data):
-            # Check if the gate is either ry or rz.
-            should_ry = True if index_inst % 2 == 0 else False
-            if should_ry:
-                assert instruction.name == "ry"
-            else:
-                assert instruction.name == "rz"
-            gates.append(instruction.name)
-
-            # Check if the current qubit is correct.
-            qubit_applied = instruction.qubits[0]._index
-            assert qubit_applied == qubit_index
-
-            # Check if the gates are in the correct order.
-            assert len(gates) <= 2
-            if len(gates) == 2:
-                assert gates == ["ry", "rz"]
-                qubit_index += 1
-                gates = []
+        new_data_dimension = data_dimension + 1
+        yz_encoder.data_dimension = new_data_dimension
+        assert isinstance(yz_encoder.parameters, list)
+        assert len(yz_encoder.parameters) == 2
+        assert isinstance(yz_encoder.parameters[0], qiskit.circuit.ParameterVector)
+        assert isinstance(yz_encoder.parameters[1], qiskit.circuit.ParameterVector)
+        if new_data_dimension % 2 == 0:  # Even
+            assert yz_encoder.num_parameters == new_data_dimension
+            assert yz_encoder.num_encoding_qubits == new_data_dimension // 2
+            assert len(yz_encoder.parameters[0]) == new_data_dimension // 2
+        else:  # Odd
+            assert yz_encoder.num_parameters == new_data_dimension + 1
+            assert yz_encoder.num_encoding_qubits == (new_data_dimension + 1) // 2
+            assert len(yz_encoder.parameters[0]) == (new_data_dimension + 1) // 2
