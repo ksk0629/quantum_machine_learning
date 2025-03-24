dc7afb3fc0d55f5df21ff7799e9a090b5d987a01
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2025-03-24 01:51:18 -0400

    #25 Update a test

--- tests/test_encoders/test_x_encoder.py
+++ tests/test_encoders/test_x_encoder.py
@@ -32,7 +32,7 @@ class TestXEncoder:
         assert isinstance(x_encoder.parameters[0], qiskit.circuit.ParameterVector)
         assert len(x_encoder.parameters[0]) == data_dimension
 
-        new_data_dimension = 10
+        new_data_dimension = data_dimension + 1
         x_encoder.data_dimension = new_data_dimension
         assert x_encoder.num_encoding_qubits == new_data_dimension
         assert x_encoder.num_parameters == new_data_dimension
