a8435bbbaa70ea6e831011d8844f2bd4b90fc640
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2025-03-24 01:31:07 -0400

    #25 Modify XEncoder and its tests to be along with the new BaseEncoder

--- quantum_machine_learning/encoders/x_encoder.py
+++ quantum_machine_learning/encoders/x_encoder.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 import qiskit
 
 from quantum_machine_learning.encoders.base_encoder import BaseEncoder
@@ -6,51 +8,28 @@ from quantum_machine_learning.encoders.base_encoder import BaseEncoder
 class XEncoder(BaseEncoder):
     """XEncoder class"""
 
-    def __init__(self, data_dimension: int, name: str = "x_encoder"):
+    def __init__(
+        self,
+        data_dimension: int,
+        name: str = "x_encoder",
+        transformer: Callable[[list[float]], list[float]] | None = None,
+    ):
         """Initialise this encoder.
 
         :param int data_dimension: the dimension of data
         :param str name: the name of the circuit
         """
-        super().__init__(name=name)
-
-        self._data_dimension = None
-        self.__parameters = None
-        self.data_dimension = data_dimension
+        super().__init__(
+            data_dimension=data_dimension, name=name, transformer=transformer
+        )
 
     @property
-    def data_dimension(self) -> int:
-        """Return the dimension of data.
+    def num_encoding_qubits(self) -> int:
+        """Return the number of qubits to be encoded.
 
-        :return int: the dimension of data
-        """
-        return self._data_dimension
-
-    @data_dimension.setter
-    def data_dimension(self, data_dimension: int):
-        """Set the new dimension of data and reset the register and parameters.
-
-        :param int data_dimension: the dimension of data
-        """
-        self._data_dimension = data_dimension
-        self._reset_register()
-        self._reset_parameters()
-
-    @property
-    def num_parameters(self) -> int:
-        """Return the number of parameters, which is the same as the number of qubits.
-
-        :return int: the number of the parameters
+        :return int: the number of encoding qubits
         """
         return self.data_dimension
-
-    @property
-    def parameters(self) -> qiskit.circuit.ParameterVector:
-        """Return the parameter vector of this circuit.
-
-        :return qiskit.circuit.ParameterVecotr: the parameter vector
-        """
-        return self.__parameters
 
     def _check_configuration(self, raise_on_failure=True) -> bool:
         """Check if the current configuration is valid.
@@ -64,14 +43,14 @@ class XEncoder(BaseEncoder):
 
     def _reset_register(self) -> None:
         """Reset the register."""
-        qreg = qiskit.QuantumRegister(self.num_parameters)
+        qreg = qiskit.QuantumRegister(self.num_encoding_qubits)
         self.qregs = [qreg]
 
     def _reset_parameters(self) -> None:
         """Reset the parameter vector."""
-        self.__parameters = qiskit.circuit.ParameterVector(
-            "x", length=self.num_parameters
-        )
+        self._parameters = [
+            qiskit.circuit.ParameterVector("x", length=self.num_encoding_qubits)
+        ]
 
     def _build(self) -> None:
         """Build the circuit."""
@@ -80,7 +59,7 @@ class XEncoder(BaseEncoder):
         # Make the quantum circuit.
         circuit = qiskit.QuantumCircuit(*self.qregs)
 
-        # Add the encoding part: the rotation X.
+        # Add the encoding part: the X-rotation.
         for index, parameter in enumerate(self.__parameters):
             circuit.rx(parameter, index)
 
--- tests/test_encoders/test_x_encoder.py
+++ tests/test_encoders/test_x_encoder.py
@@ -16,21 +16,25 @@ class TestXEncoder:
         Create an XEcnoder instance and chenge the data dimension.
 
         Check if
-        - its data_dimension is the same as the given data_dimension.
+        - its num_encoding_qubits is the same as the given data_dimension.
         - its num_parameters is the same as the given data_dimension.
         - its parameters is an instance of qiskit.circuit.ParameterVector.
         - the length of its parameters is the same as the given data_dimension.
         - the above four things are preserved with the new_data_dimension after substituting new_data_dimension.
         """
         x_encoder = XEncoder(data_dimension=data_dimension)
-        assert x_encoder.data_dimension == data_dimension
+        assert x_encoder.num_encoding_qubits == data_dimension
         assert x_encoder.num_parameters == data_dimension
-        assert isinstance(x_encoder.parameters, qiskit.circuit.ParameterVector)
-        assert len(x_encoder.parameters) == data_dimension
+        assert isinstance(x_encoder.parameters, list)
+        assert len(x_encoder.parameters) == 1
+        assert isinstance(x_encoder.parameters[0], qiskit.circuit.ParameterVector)
+        assert len(x_encoder.parameters[0]) == data_dimension
 
         new_data_dimension = 10
         x_encoder.data_dimension = new_data_dimension
-        assert x_encoder.data_dimension == new_data_dimension
+        assert x_encoder.num_encoding_qubits == new_data_dimension
         assert x_encoder.num_parameters == new_data_dimension
-        assert isinstance(x_encoder.parameters, qiskit.circuit.ParameterVector)
-        assert len(x_encoder.parameters) == new_data_dimension
+        assert isinstance(x_encoder.parameters, list)
+        assert len(x_encoder.parameters) == 1
+        assert isinstance(x_encoder.parameters[0], qiskit.circuit.ParameterVector)
+        assert len(x_encoder.parameters[0]) == new_data_dimension
