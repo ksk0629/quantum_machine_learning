e69de2525c4b21f585bd8f5771d59b7783185775
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2025-03-25 17:06:51 -0400

    #27 Update BaseLayer

--- quantum_machine_learning/layers/base_layer.py
+++ quantum_machine_learning/layers/base_layer.py
@@ -3,21 +3,39 @@ from abc import ABC, abstractmethod
 import qiskit
 
 
-class BaseLayer(ABC):
+class BaseLayer(qiskit.circuit.library.BlueprintCircuit, ABC):
     """BaseLayer abstract class, which is all quantum layers inherit this."""
 
-    def __init__(self):
-        pass
+    def __init__(self, *regs, num_state_qubits: int, name: str | None = None):
+        """Initialise the BaseLayer.
+
+        :param int num_state_qubits: the number of state qubits
+        :param str | None name: the name of this encoder, defaults to None
+        """
+        self._num_state_qubits = None
+
+        super().__init__(*regs, name=name)
+
+        self.num_state_qubits = num_state_qubits
+
+    @property
+    def num_state_qubits(self) -> int:
+        """Return the number of the state qubits
+
+        :return int: the number of the state qubits
+        """
+        return self._num_state_qubits
+
+    @num_state_qubits.setter
+    def num_state_qubits(self, num_state_qubits: int) -> None:
+        """Set the new number of the state qubits and reset the register.
+
+        :param int num_state_qubits: the new number of of the state qubits
+        """
+        self._num_state_qubits = num_state_qubits
+        self._reset_register()
 
     @abstractmethod
-    def get_circuit(self) -> qiskit.QuantumCircuit:
+    def _reset_register(self) -> None:
+        """Reset the register."""
         pass
-
-    def __call__(self) -> qiskit.QuantumCircuit:
-        return self.get_circuit()
-
-    @classmethod
-    def get(cls, *args) -> qiskit.QuantumCircuit:
-        layer = cls(*args)
-        circuit = layer.get_circuit()
-        return circuit
