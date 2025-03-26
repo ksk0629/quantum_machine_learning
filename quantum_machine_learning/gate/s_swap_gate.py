238b1a6b5083f18411504fbcb158c6bef7eb959e
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2025-03-26 14:40:14 -0400

    #34 Go along with mypy

--- quantum_machine_learning/gate/s_swap_gate.py
+++ quantum_machine_learning/gate/s_swap_gate.py
@@ -4,16 +4,22 @@ import qiskit
 class SSwapGate(qiskit.circuit.Gate):
     """Square root swap gate class."""
 
-    def __init__(self, label: str | None = None, *, duration=None, unit="dt"):
+    def __init__(
+        self,
+        label: str | None = None,
+        *,
+        duration: int | float | None = None,
+        unit: str = "dt"
+    ):
         """Create the square root of swap gate."""
         super().__init__("√SWAP", 2, [], label=label, duration=duration, unit=unit)
 
-    def _define(self):
+    def _define(self) -> None:
         """Calculate a subcircuit that implements this unitary."""
 
         q = qiskit.QuantumRegister(2, "q")
         qc = qiskit.QuantumCircuit(q, name=self.name)
-        rules = [
+        rules: list[tuple[qiskit.circuit.Gate, list[int], list[int]]] = [
             (qiskit.circuit.library.CXGate(), [q[0], q[1]], []),
             (qiskit.circuit.library.CSXGate(), [q[1], q[0]], []),
             (qiskit.circuit.library.CXGate(), [q[0], q[1]], []),
