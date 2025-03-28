1005c72c7e9d9b795c09b936f918ce740bdcbe64
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2025-03-28 14:40:30 -0400

    #32 Update QuClassi's tests

--- tests/test_quclassi/test_quclassi.py
+++ tests/test_quclassi/test_quclassi.py
@@ -6,6 +6,7 @@ import tempfile
 import qiskit
 import qiskit_aer
 import pytest
+import yaml
 
 from quantum_machine_learning.quclassi.quclassi import QuClassi
 
@@ -214,19 +215,62 @@ class TestQuClassi:
         backend = qiskit_aer.AerSimulator(seed_simulator=901)
         shots = 8192
 
-        predicted_labels = quclassi.classify(data=data, backend=backend, shots=shots)
+        predicted_labels = quclassi.classify(
+            data=data, backend=backend, shots=shots, optimisation_level=0
+        )
         assert isinstance(predicted_labels, list)
         assert len(predicted_labels) == len(data)
         assert all(label in labels for label in predicted_labels)
 
     @pytest.mark.quclassi
-    def test_invalid_classify(self):
+    def test_invalid_parameter_classify(self):
         """Abnormal test;
         run classify method without setting the parameter values.
 
         Check if AttributeError happens.
         """
-        pass
+        classical_data_size = 3
+        structure = "s"
+        labels = ["a", "b"]
+        quclassi = QuClassi(
+            classical_data_size=classical_data_size,
+            structure=structure,
+            labels=labels,
+        )
+
+        data = [[1, 2, 3, 0], [4, 5, 6, 0], [7, 8, 9, 0]]
+        backend = qiskit_aer.AerSimulator(seed_simulator=901)
+        shots = 8192
+        with pytest.raises(AttributeError):
+            quclassi.classify(
+                data=data, backend=backend, shots=shots, optimisation_level=0
+            )
+
+    @pytest.mark.quclassi
+    def test_invalid_data_classify(self):
+        """Abnormal test;
+        run classify method by giving incorrect data.
+
+        Check if AttributeError happens.
+        """
+        classical_data_size = 3
+        structure = "s"
+        labels = ["a", "b"]
+        initial_parameters = {"a": [1, 2, 3, 4], "b": [3, 4, 5, 6]}
+        quclassi = QuClassi(
+            classical_data_size=classical_data_size,
+            structure=structure,
+            labels=labels,
+            initial_parameters=initial_parameters,
+        )
+
+        data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        backend = qiskit_aer.AerSimulator(seed_simulator=901)
+        shots = 8192
+        with pytest.raises(ValueError):
+            quclassi.classify(
+                data=data, backend=backend, shots=shots, optimisation_level=0
+            )
 
     @pytest.mark.quclassi
     def test_save(self):
@@ -242,6 +286,29 @@ class TestQuClassi:
             - initial_parameters
             - name
         """
+        classical_data_size = 3
+        structure = "s"
+        labels = ["a", "b"]
+        initial_parameters = {"a": [1, 2, 3, 4], "b": [3, 4, 5, 6]}
+        quclassi = QuClassi(
+            classical_data_size=classical_data_size,
+            structure=structure,
+            labels=labels,
+            initial_parameters=initial_parameters,
+        )
+        with tempfile.TemporaryDirectory() as tmp_dir_path:
+            quclassi.save(model_dir_path=tmp_dir_path)
+
+            yaml_path = os.path.join(tmp_dir_path, QuClassi.MODEL_FILENAME)
+            assert os.path.isfile(yaml_path)
+
+            with open(yaml_path, "r", encoding=QuClassi.ENCODING) as yaml_file:
+                quclassi_info = yaml.safe_load(yaml_file)
+            assert "classical_data_size" in quclassi_info
+            assert "structure" in quclassi_info
+            assert "labels" in quclassi_info
+            assert "initial_parameters" in quclassi_info
+            assert "name" in quclassi_info
 
     @pytest.mark.quclassi
     def test_invalid_save(self):
@@ -250,6 +317,17 @@ class TestQuClassi:
 
         Check if AttributeError happens.
         """
+        classical_data_size = 3
+        structure = "s"
+        labels = ["a", "b"]
+        quclassi = QuClassi(
+            classical_data_size=classical_data_size,
+            structure=structure,
+            labels=labels,
+        )
+        with tempfile.TemporaryDirectory() as tmp_dir_path:
+            with pytest.raises(AttributeError):
+                quclassi.save(model_dir_path=tmp_dir_path)
 
     @pytest.mark.quclassi
     def test_load(self):
@@ -258,3 +336,27 @@ class TestQuClassi:
 
         Check if the loaded QuClassi has the same class variables as the given yaml file.
         """
+        classical_data_size = 3
+        structure = "s"
+        labels = ["a", "b"]
+        initial_parameters = {"a": [1, 2, 3, 4], "b": [3, 4, 5, 6]}
+        name = "Name"
+        yaml_data = {
+            "classical_data_size": classical_data_size,
+            "structure": structure,
+            "labels": labels,
+            "initial_parameters": initial_parameters,
+            "name": name,
+        }
+
+        with tempfile.TemporaryDirectory() as tmp_dir_path:
+            yaml_path = os.path.join(tmp_dir_path, QuClassi.MODEL_FILENAME)
+            with open(yaml_path, "w", encoding=QuClassi.ENCODING) as yaml_file:
+                yaml.dump(yaml_data, yaml_file, default_flow_style=False)
+
+            quclassi = QuClassi.load(tmp_dir_path)
+            assert quclassi.classical_data_size == classical_data_size
+            assert quclassi.structure == structure
+            assert quclassi.labels == labels
+            assert quclassi.parameter_values == initial_parameters
+            assert quclassi.name == name
