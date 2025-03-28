7a511f33b8e203f58cf98477529b854f5b8847f7
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2025-03-28 11:13:52 -0400

    #32 Update tests

--- tests/test_quclassi/test_quclassi.py
+++ tests/test_quclassi/test_quclassi.py
@@ -1,7 +1,8 @@
+import itertools
 import os
+import string
 import tempfile
 
-import numpy as np
 import qiskit
 import pytest
 
@@ -13,16 +14,33 @@ class TestQuClassi:
     def setup_class(cls):
         pass
 
+    @pytest.mark.quclassi
     def test_init(self):
         """Normal test;
         create an instance of QuClassi with valid arguments.
 
         Check if
         - its classical_data_size is the same as the given classical_data_size.
+        - its using_classical_data_size is the nearest larger even number of classical_data_size.
+        - its num_data_qubits is the half of its using_classical_data_size.
+        - its num_train_qubits is the half of its using_classical_data_size.
         - its structure is the same as the given structure.
         - its labels are the same as the given labels.
         - its parameter values are the same as the given initial_parameters.
-        - the things above are correct after substituting a new values.
+        - its classical_data_size is the same as the given new classical_data_size
+          after substituting a new classical_data_size.
+        - its using_classical_data_size is the nearest larger even number of
+          new classical_data_size after substituting a new classical_data_size.
+        - its num_data_qubits is the half of its new using_classical_data_size
+          after substituting a new classical_data_size.
+        - its num_train_qubits is the half of its new using_classical_data_size
+          after substituting a new classical_data_size.
+        - its structure is the same as the given new structure
+          after substituting a new structure.
+        - its labels is the same as the given new labels after substituting a new labels.
+        - its parameter values are empty dict after substituting a new structure.
+        - its parameter values are the same as the new parameter values
+          after substituting new parameter values.
         - the type of its trainable_parameters is qiskit.circuit.ParameterExpression.
         - the type of its data_parameters is qiskit.circuit.ParameterExpression.
         """
@@ -37,6 +55,9 @@ class TestQuClassi:
             initial_parameters=initial_parameters,
         )
         assert quclassi.classical_data_size == classical_data_size
+        assert quclassi.using_classical_data_size == classical_data_size + 1
+        assert quclassi.num_data_qubits == quclassi.using_classical_data_size // 2
+        assert quclassi.num_train_qubits == quclassi.using_classical_data_size // 2
         assert quclassi.structure == structure
         assert quclassi.labels == labels
         assert quclassi.parameter_values == initial_parameters
@@ -44,6 +65,9 @@ class TestQuClassi:
         new_classical_data_size = classical_data_size + 1
         quclassi.classical_data_size = new_classical_data_size
         assert quclassi.classical_data_size == new_classical_data_size
+        assert quclassi.using_classical_data_size == new_classical_data_size
+        assert quclassi.num_data_qubits == quclassi.using_classical_data_size // 2
+        assert quclassi.num_train_qubits == quclassi.using_classical_data_size // 2
 
         new_structure = structure + "d" + "e"
         quclassi.structure = new_structure
@@ -52,192 +76,114 @@ class TestQuClassi:
         new_labels = labels + ["c"]
         quclassi.labels = new_labels
         assert quclassi.labels == new_labels
+        assert quclassi.parameter_values == dict()
 
-    @pytest.mark.parametrize(
-        "structure",
-        ["s", "d", "e", "sd", "se", "de", "sde", "sss", "dseds"],
-    )
-    def test_build_with_valid_structure(self, structure):
-        """Normal test;
-        Run build function of self.quclassi with various structure.
+        new_parameter_values = initial_parameters
+        new_parameter_values["c"] = [[7, 8], [9, 10]]
+        quclassi.parameter_values = new_parameter_values
+        assert quclassi.parameter_values == new_parameter_values
 
-        Check if
-        - self.quclassi.circuit is an instance of qiskit.QuantumCircuit.
-        - self.quclassi.trainable_parameters is qiskit.circuit.parametertable.ParameterView.
-        - self.quclassi.data_parameters is qiskit.circuit.parametertable.ParameterView.
-        - the length of self.quclassi.trainable_parameters is suitable,
-          which varies depending on structure.
-        - the length of self.quclassi.data_parameters is as same as
-          self.quclassi.num_data_qubits * 2.
-        """
-        self.quclassi.build(structure)
-
-        assert isinstance(self.quclassi.circuit, qiskit.QuantumCircuit)
         assert isinstance(
-            self.quclassi.trainable_parameters,
-            qiskit.circuit.parametertable.ParameterView,
+            quclassi.trainable_parameters, qiskit.circuit.ParameterExpression
         )
-        assert isinstance(
-            self.quclassi.data_parameters, qiskit.circuit.parametertable.ParameterView
-        )
-        assert len(self.quclassi.data_parameters) == self.quclassi.num_data_qubits * 2
+        assert isinstance(quclassi.data_parameters, qiskit.circuit.ParameterExpression)
 
-        # Check the number of trainable_parameters
-        num_trainable_parameters = 0
-        num_trainable_parameters += (
-            structure.count("s") * self.quclassi.num_train_qubits
-        ) * 2
-        num_trainable_parameters += (
-            structure.count("d") * (self.quclassi.num_train_qubits - 1) * 2
-        )
-        num_trainable_parameters += (
-            structure.count("e") * (self.quclassi.num_train_qubits - 1) * 2
-        )
-        assert len(self.quclassi.trainable_parameters) == num_trainable_parameters
-
+    @pytest.mark.quclassi
     @pytest.mark.parametrize(
-        "structure",
-        ["as", "sac", "sdev", "a"],
+        "all_letters",
+        [
+            "s",
+            "d",
+            "e",
+            "sd",
+            "se",
+            "de",
+            "sde",
+            "ssde",
+            "sdde",
+            "sdee",
+            "ssdde",
+            "sddee",
+            "ssddee",
+        ],
     )
-    def test_build_with_invalid_structure(self, structure):
+    def test_valid_structure(self, all_letters):
+        """Normal test;
+        Create an instance of QuClassi with several valid structures.
+
+        Check if its structure is the same as the given structure.
+        """
+        classical_data_size = 3
+        labels = ["a", "b"]
+        for conb in itertools.permutations(all_letters):
+            for structure_letters in conb:
+                structure = "".join(structure_letters)
+                quclassi = QuClassi(
+                    classical_data_size=classical_data_size,
+                    structure=structure,
+                    labels=labels,
+                )
+                assert quclassi.structure == structure
+
+    @pytest.mark.quclassi
+    @pytest.mark.parametrize(
+        "all_letters",
+        [
+            "s",
+            "d",
+            "e",
+            "sd",
+            "se",
+            "de",
+            "sde",
+            "ssde",
+            "sdde",
+            "sdee",
+            "ssdde",
+            "sddee",
+            "ssddee",
+        ],
+    )
+    def test_invalid_structure(self, all_letters):
+        """Abormal test;
+        Create an instance of QuClassi with several invalid structures.
+
+        Check if AttributeError happens.
+        """
+        classical_data_size = 3
+        labels = ["a", "b"]
+        for i in string.ascii_letters:  # Loop for all letters.
+            if i == "s" or i == "d" or i == "e":
+                # Continue if i is one of the valid letters.
+                continue
+            invalid_all_letters = all_letters + i
+            for conb in itertools.permutations(invalid_all_letters):
+                for structure_letters in conb:
+                    structure = str(structure_letters)
+                    with pytest.raises(AttributeError):
+                        QuClassi(
+                            classical_data_size=classical_data_size,
+                            structure=structure,
+                            labels=labels,
+                        )
+
+    @pytest.mark.quclassi
+    @pytest.mark.parametrize("labels", [None, ["b"]])
+    def test_invalid_parameter_values(self, labels):
         """Abnormal test;
-        Run build function of self.quclassi with various structure.
+        set invalid parameter values.
 
-        Case 1: structure contains any letters other than s, d or e.
-        Check if VallueError happens.
+        Check if AttributeError happens.
         """
-        with pytest.raises(ValueError):
-            self.quclassi.build(structure)
-
-    def test_save(self):
-        """Normal test;
-        Run save function.
-
-        Check if
-        - there is basic_info.pkl.
-        - there is circuit.qpy.
-        - there is trainable_parameters.pkl.
-        - there is data_parameters.pkl.
-        - there is trained_parameters.pkl.
-        - save does not work if there is the existing directory.
-        """
-        self.quclassi.build(self.structure)
-        self.quclassi.save(self.model_dir_path)
-
-        basic_info_path = os.path.join(self.model_dir_path, "basic_info.pkl")
-        assert os.path.isfile(basic_info_path)
-        os.remove(basic_info_path)
-
-        circuit_path = os.path.join(self.model_dir_path, "circuit.qpy")
-        assert os.path.isfile(circuit_path)
-        os.remove(circuit_path)
-
-        trainable_parameters_path = os.path.join(
-            self.model_dir_path, "trainable_parameters.pkl"
+        classical_data_size = 3
+        structure = "s"
+        quclassi = QuClassi(
+            classical_data_size=classical_data_size, labels=labels, structure=structure
         )
-        assert os.path.isfile(trainable_parameters_path)
-        os.remove(trainable_parameters_path)
 
-        data_parameters_path = os.path.join(self.model_dir_path, "data_parameters.pkl")
-        assert os.path.isfile(data_parameters_path)
-        os.remove(data_parameters_path)
-        trained_parameters_path = os.path.join(
-            self.model_dir_path, "trained_parameters.pkl"
-        )
-        assert os.path.isfile(trained_parameters_path)
-        os.remove(trained_parameters_path)
-
-        with pytest.raises(OSError):
-            self.quclassi.save(self.model_dir_path)
-
-        os.rmdir(self.model_dir_path)
-
-    def test_load(self):
-        """Normal test;
-        Run load.
-
-        Check if the loaded QuClassi instance is the same as the quclassi that is saved.
-        """
-        self.quclassi.build(self.structure)
-        self.quclassi.save(self.model_dir_path)
-
-        loaded_quclassi = QuClassi.load(self.model_dir_path)
-        assert self.quclassi.classical_data_size == loaded_quclassi.classical_data_size
-        assert self.quclassi.labels == loaded_quclassi.labels
-        assert self.quclassi.circuit == loaded_quclassi.circuit
-        assert (
-            self.quclassi.trainable_parameters == loaded_quclassi.trainable_parameters
-        )
-        assert self.quclassi.data_parameters == loaded_quclassi.data_parameters
-        assert self.quclassi.trained_parameters == loaded_quclassi.trained_parameters
-
-        basic_info_path = os.path.join(self.model_dir_path, "basic_info.pkl")
-        os.remove(basic_info_path)
-
-        circuit_path = os.path.join(self.model_dir_path, "circuit.qpy")
-        os.remove(circuit_path)
-
-        trainable_parameters_path = os.path.join(
-            self.model_dir_path, "trainable_parameters.pkl"
-        )
-        os.remove(trainable_parameters_path)
-
-        data_parameters_path = os.path.join(self.model_dir_path, "data_parameters.pkl")
-        os.remove(data_parameters_path)
-        trained_parameters_path = os.path.join(
-            self.model_dir_path, "trained_parameters.pkl"
-        )
-        os.remove(trained_parameters_path)
-        os.rmdir(self.model_dir_path)
-
-    def test_get_fidelities_without_trained_parameters(self):
-        """Abnormal test;
-        Run get_fidelities function without setting the trained parameters.
-
-        Check if ValueError happens.
-        """
-        if self.quclassi.trained_parameters is not None:
-            self.quclassi.trained_parameters = None
-        with pytest.raises(ValueError):
-            self.quclassi.get_fidelities(self.data)
-
-    def test_get_fidelities_with_trained_parameters(self):
-        """Normal test;
-        Run get_fidelities function after setting the trained_parameters.
-
-        Check if
-        - the type of the return value is dict.
-        - the return value's keys coincide with self.quclassi.labels.
-        - the return value's values are between 0 and 1.
-        """
-        self.quclassi.trained_parameters = self.trained_parameters
-        fidelities = self.quclassi.get_fidelities(self.data)
-        # Check the type of fidelieis.
-        assert isinstance(fidelities, dict)
-        # Check the keys of fidelities concide with self.quclassi.labels.
-        assert set(self.quclassi.labels) == set(fidelities.keys())
-        assert len(self.quclassi.labels) == len(fidelities.keys())
-        # Check the values of fidelities are between 0 and 1.
-        for fidelity in fidelities.values():
-            assert 0 <= fidelity <= 1
-
-    def test_classify_without_trained_parameters(self):
-        """Abnormal test;
-        Run classify function without setting the trained parameters.
-
-        Check if ValueError happens.
-        """
-        if self.quclassi.trained_parameters is not None:
-            self.quclassi.trained_parameters = None
-        with pytest.raises(ValueError):
-            self.quclassi.classify(self.data)
-
-    def test_classify_with_trained_parameters(self):
-        """Normal test;
-        Run classify function after setting the trained_parameters.
-
-        Check if the returned value is in self.labels.
-        """
-        self.quclassi.trained_parameters = self.trained_parameters
-        label_classified = self.quclassi.classify(self.data)
-        assert label_classified in self.labels
+        parameter_values = {"a": [[1, 2], [3, 4]]}
+        with pytest.raises(AttributeError):
+            # If labels is None, AttributeError must happen.
+            # Also, if the keys of the parameter values are not the same labels,
+            # Attribute Error must happens.
+            quclassi.parameter_values = parameter_values
