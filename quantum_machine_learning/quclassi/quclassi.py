0b22bb539801618e18ccccda8f6135aa020e2e69
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2025-03-28 09:47:20 -0400

    #32 Add functions to classify data

--- quantum_machine_learning/quclassi/quclassi.py
+++ quantum_machine_learning/quclassi/quclassi.py
@@ -1,3 +1,4 @@
+import dataclasses
 import os
 import pickle
 from typing import Final
@@ -6,6 +7,7 @@ import numpy as np
 import qiskit
 from qiskit import qpy, primitives
 import qiskit.circuit
+import yaml
 
 from quantum_machine_learning.encoders.yz_encoder import YZEncoder
 from quantum_machine_learning.layers.single_qubit_unitary_layer import (
@@ -23,6 +25,15 @@ from quantum_machine_learning.postprocessor.postprocessor import Postprocessor
 from quantum_machine_learning.utils.circuit_utils import CircuitUtils
 
 
+@dataclasses.dataclass
+class QuClassiInfo:
+    classical_data_size: int
+    structure: str
+    labels: list[str]
+    initial_parameters: dict[str, list[float]] | None = None
+    name: str | None = None
+
+
 class QuClassi(qiskit.circuit.library.BlueprintCircuit):
     """QuClassi class."""
 
@@ -35,6 +46,12 @@ class QuClassi(qiskit.circuit.library.BlueprintCircuit):
         dual_qubit_unitary,
         entanglement_unitary,
     ]
+    # Define the classical register name.
+    creg_name: Final[str] = "creg"
+    # Define the model file name to save and load.
+    model_filename: Final[str] = "model.yaml"
+    # Define the encoding method to save and load.
+    encoding: Final[str] = "utf-8"
 
     def __init__(
         self,
@@ -175,11 +192,11 @@ class QuClassi(qiskit.circuit.library.BlueprintCircuit):
 
     @parameter_values.setter
     def parameter_values(self, parameter_values: dict[str, list[float]]) -> None:
-        """Set the new parameter values.
+        """Set the new parameter values in the same order as the labels.
 
         :param dict[str, list[float]] parameter_values: a new parameter values
         :raises AttributeError: if the labels hasn't been set yet
-        :raises AttributeError: if the keys of the new parameter values are not in the labels.
+        :raises AttributeError: if the keys of the new parameter values are not the same as the labels.
         """
         if self.labels is None:
             raise AttributeError(
@@ -187,14 +204,16 @@ class QuClassi(qiskit.circuit.library.BlueprintCircuit):
             )
 
         if parameter_values is not None:
-            for parameter_key in parameter_values:
-                if parameter_key not in self.labels:
-                    raise AttributeError(
-                        f"""
-                        All the keys of parameter_values must be in the labels: {self.labels}.
-                        However, {parameter_key} is given.
-                        """
-                    )
+            if set(parameter_values.keys()) != set(self.labels):
+                raise AttributeError(
+                    f"""
+                    All the keys of parameter_values must be the same as the labels: {self.labels}.
+                    However, {parameter_values.keys()} is given.
+                    """
+                )
+
+            # Sort by the labels.
+            parameter_values = {label: parameter_values[label] for label in self.labels}
 
         self._parameter_values = parameter_values
 
@@ -319,153 +338,98 @@ class QuClassi(qiskit.circuit.library.BlueprintCircuit):
 
         self.append(circuit.to_gate(), self.qubits)
 
-    # def __call__(
-    #     self,
-    #     data: np.ndarray,
-    #     sampler: (
-    #         primitives.BaseSamplerV1 | primitives.BaseSamplerV2
-    #     ) = primitives.StatevectorSampler(seed=901),
-    #     shots: int = 8096,
-    # ) -> str:
-    #     """Call the classify function.
+    def _classify_datum(
+        self,
+        datum: list[float],
+        backend: qiskit.providers.Backend,
+        shots: int = 9010,
+    ) -> str:
+        """Classify one datum and return the label name.
 
-    #     :param np.ndarray data: input data, which is preprocessed if needed
-    #     :param qiskit.primitives.BaseSamplerV1  |  qiskit.primitives.BaseSamplerV2 sampler: sampler primitives, defaults to qiskit.primitives.StatevectorSampler
-    #     :param int shots: number of shots
-    #     :return str: predicted label
-    #     """
-    #     return self.classify(data=data, sampler=sampler, shots=shots)
+        :param list[float] datum: a datum to be classified
+        :param qiskit.providers.Backend backend: a backend
+        :param int shots: the number of shots, defaults to 9010
+        :raises AttributeError: if the parameter values haven't been set
+        :return str: the predicted label
+        """
+        # Raise the error if the parameter values haven't been set.
+        if self.parameter_values is None:
+            error_msg = "No parameter values are found. Set the parameter values first."
+            raise AttributeError(error_msg)
 
-    # def get_fidelities(
-    #     self,
-    #     data: np.ndarray,
-    #     sampler: (
-    #         primitives.BaseSamplerV1 | primitives.BaseSamplerV2
-    #     ) = primitives.StatevectorSampler(seed=901),
-    #     shots: int = 1024,
-    # ) -> dict[str, np.ndarray]:
-    #     """Get fidelities between the input data and each representative state of self.labels.
+        # Build the circuit if not yet.
+        if not self._is_build:
+            self._build()
 
-    #     :param np.ndarray data: input data, which is preprocessed if needed
-    #     :param qiskit.primitives.BaseSamplerV1  |  qiskit.primitives.BaseSamplerV2 sampler: sampler primitives, defaults to qiskit.primitives.StatevectorSampler
-    #     :param int shots: number of shots
-    #     :raises ValueError: if self.trained_parameters was not set.
-    #     :return str: predicted label
-    #     :return dict[str, np.ndarray]: fidelities
-    #     """
-    #     if self.trained_parameters is None:
-    #         msg = "There is not trained_parameters set."
-    #         raise ValueError(msg)
+        # Create a circuit with measurement.
+        creg = qiskit.ClassicalRegister(1, name=QuClassi.creg_name)
+        circuit = self.copy()
+        circuit.add_register(creg)
+        circuit.measure(self._ancilla_qreg, creg)
 
-    #     # Set data as the data_parameters.
-    #     data_parameters = CircuitUtils.get_parameter_dict(
-    #         parameter_names=self.data_parameters, parameters=data
-    #     )
+        # Load the data to the parameters.
+        data_parameters = {
+            data_parameter: d for data_parameter, d in zip(self.data_parameters, datum)
+        }
 
-    #     # Create the combination of the circuit and parameters to run the circuits.
-    #     pubs = []
-    #     for trained_parameters in self.trained_parameters:
-    #         parameters = CircuitUtils.get_parameter_dict(
-    #             parameter_names=self.trainable_parameters, parameters=trained_parameters
-    #         )
-    #         parameters = {**parameters, **data_parameters}
-    #         pubs.append((self.circuit, parameters))
+        # Create primitive unified blocks.
+        #  [0] will correspond to the first label
+        #  ...
+        #  [n] will correspond to the n-1 label ...
+        primitive_unified_blocks = []  # [0] -> label
+        for parameter_values in self.parameter_values.values():
+            # Set the parameter values.
+            trained_parameters = {
+                trainable_parameter: parameter_value
+                for trainable_parameter, parameter_value in zip(
+                    self.trainable_parameters, parameter_values
+                )
+            }
+            # Add this primitive unified block to the dict.
+            primitive_unified_block = (
+                circuit,
+                {**trained_parameters, **data_parameters},
+            )
+            primitive_unified_blocks.append(primitive_unified_block)
 
-    #     # Run the sampler.
-    #     job = sampler.run(pubs, shots=shots)
+        # Transplie the backend.
+        pass_manager = qiskit.transpiler.generate_preset_pass_manager(
+            optimization_level=3, backend=backend
+        )
+        # Run the circuits.
+        jobs = pass_manager.run(primitive_unified_blocks, shots=shots)
 
-    #     # Calculate the fidelities.
-    #     fidelities = {}
-    #     results = job.result()
-    #     for result, label in zip(results, self.labels):
-    #         fidelities[label] = Postprocessor.calculate_fidelity_from_swap_test(
-    #             result.data.c.get_counts()
-    #         )
+        # Get the fidelities corresponding to each label.
+        fidelities = {}
+        results = jobs.result()
+        for result, label in zip(results, self.labels):
+            fidelities[label] = Postprocessor.calculate_fidelity_from_swap_test(
+                result.data.creg.get_counts()  # creg is after QuClassi.creg_name
+            )
 
-    #     return fidelities
+        # Find the label whose value is the larest fidelity.
+        predicted_label = max(fidelities, key=fidelities.get)
 
-    # def classify(
-    #     self,
-    #     data: np.ndarray,
-    #     sampler: (
-    #         primitives.BaseSamplerV1 | primitives.BaseSamplerV2
-    #     ) = primitives.StatevectorSampler(seed=901),
-    #     shots: int = 1024,
-    # ) -> str:
-    #     """Classify the input data into one of self.labels.
+        return predicted_label
 
-    #     :param np.ndarray data: input data, which is preprocessed if needed
-    #     :param qiskit.primitives.BaseSamplerV1  |  qiskit.primitives.BaseSamplerV2 sampler: sampler primitives, defaults to qiskit.primitives.StatevectorSampler
-    #     :param int shots: number of shots
-    #     :raises ValueError: if self.trained_parameters was not set.
-    #     :return str: predicted label
-    #     """
-    #     if self.trained_parameters is None:
-    #         msg = "There is not trained_parameters set."
-    #         raise ValueError(msg)
+    def classify(
+        self,
+        data: list[list[float]],
+        backend: qiskit.providers.Backend,
+        shots: int = 9010,
+    ) -> list[str]:
+        """Classify the given data.
 
-    #     # Get the fidelities.
-    #     fidelities = self.get_fidelities(data=data, sampler=sampler, shots=shots)
-    #     # Find the label whose value is the maximal.
-    #     label = max(fidelities, key=fidelities.get)
+        :param list[list[float]] data: data to be classcified
+        :param qiskit.providers.Backend backend: a backend
+        :param int shots: the number of shots, defaults to 9010
+        :return list[str]: the classified labels
+        """
+        # Classify each datum.
+        predicted_labels = []
+        for datum in data:
+            predicted_labels.append(
+                self._classify_datum(datum=datum, backend=backend, shots=shots)
+            )
 
-    #     return label
-
-    # def save(self, model_dir_path: str):
-    #     """Save the circuit and parameters to the directory specified by the given model_dir_path.
-
-    #     :param str model_dir_path: path to the output directory.
-    #     """
-    #     # Create the directory specified by the argument output_dir_path.
-    #     os.makedirs(model_dir_path)
-
-    #     # Create a path getter.
-    #     path_getter = QuClassiPathGetter(dir_path=model_dir_path)
-
-    #     # Save the basic information of this QuClassi.
-    #     basic_info = {
-    #         "classical_data_size": self.classical_data_size,
-    #         "labels": self.labels,
-    #     }
-    #     with open(path_getter.basic_info, "wb") as pkl_file:
-    #         pickle.dump(basic_info, pkl_file)
-
-    #     # Save the circuit.
-    #     with open(path_getter.circuit, "wb") as qpy_file:
-    #         qpy.dump(self.circuit, qpy_file)
-
-    #     # Save the parameters.
-    #     with open(path_getter.trainable_parameters, "wb") as pkl_file:
-    #         pickle.dump(self.trainable_parameters, pkl_file)
-    #     with open(path_getter.data_parameters, "wb") as pkl_file:
-    #         pickle.dump(self.data_parameters, pkl_file)
-    #     with open(path_getter.trained_parameters, "wb") as pkl_file:
-    #         pickle.dump(self.trained_parameters, pkl_file)
-
-    # @classmethod
-    # def load(cls, model_dir_path: str):
-    #     """Load the circuit and parameters from the directory specified by the given model_dir_path.
-
-    #     :param str model_dir_path: path to the input directory.
-    #     """
-    #     # Create a path getter.
-    #     path_getter = QuClassiPathGetter(dir_path=model_dir_path)
-
-    #     # Load the basic information.
-    #     with open(path_getter.basic_info, "rb") as pkl_file:
-    #         basic_info = pickle.load(pkl_file)
-    #     loaded_quclassi = cls(**basic_info)
-
-    #     # Load the circuit.
-    #     with open(path_getter.circuit, "rb") as qpy_file:
-    #         loaded_quclassi.circuit = qpy.load(qpy_file)[0]
-
-    #     # Load the parameters.
-    #     with open(path_getter.trainable_parameters, "rb") as pkl_file:
-    #         loaded_quclassi.trainable_parameters = pickle.load(pkl_file)
-    #     with open(path_getter.data_parameters, "rb") as pkl_file:
-    #         loaded_quclassi.data_parameters = pickle.load(pkl_file)
-    #     with open(path_getter.trained_parameters, "rb") as pkl_file:
-    #         loaded_quclassi.trained_parameters = pickle.load(pkl_file)
-
-    #     return loaded_quclassi
+        return predicted_labels
