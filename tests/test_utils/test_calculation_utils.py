2599c9f783893709f2c7e9c3d7186397a5a7a59a
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2025-03-29 23:23:06 -0400

    Implement tests

--- tests/test_utils/test_calculation_utils.py
+++ tests/test_utils/test_calculation_utils.py
@@ -17,7 +17,17 @@ class TestCalculationUtils:
 
         Check if the return value is 0.
         """
-        pass
+        probabilities_list = [
+            {"A": 1, "B": 0, "C": 0},
+            {"A": 0, "B": 1, "C": 0},
+            {"A": 0, "B": 0, "C": 1},
+        ]
+        true_labels = ["A", "B", "C"]
+        cross_entropy = CalculationUtils.calculate_cross_entropy(
+            probabilities_list=probabilities_list,
+            true_labels=true_labels,
+        )
+        assert cross_entropy == 0
 
     @pytest.mark.utils
     def test_calculate_accuracy(self):
@@ -26,25 +36,41 @@ class TestCalculationUtils:
 
         Check if the return value is correct.
         """
-        pass
+        predicted_labels = ["A", "B", "C"]
+        true_labels = ["B", "C", "A"]
+        shold_be_zero = CalculationUtils.calculate_accuracy(
+            predicted_labels=predicted_labels, true_labels=true_labels
+        )
+        assert shold_be_zero == 0
+
+        predicted_labels = ["A", "B", "C", "D"]
+        true_labels = ["B", "C", "C", "D"]
+        shold_be_half = CalculationUtils.calculate_accuracy(
+            predicted_labels=predicted_labels, true_labels=true_labels
+        )
+        assert shold_be_half == 0.5
+
+        predicted_labels = ["A", "B", "C", "D"]
+        true_labels = ["A", "B", "C", "D"]
+        shold_be_one = CalculationUtils.calculate_accuracy(
+            predicted_labels=predicted_labels, true_labels=true_labels
+        )
+        assert shold_be_one == 1
 
     @pytest.mark.utils
-    def test_safe_log_e_with_minus_1(self):
+    def test_safe_log_e(self):
         """Normal test;
-        run sage_log_e with 1e-1.
+        run sage_log_e with e^(-1) and 0.
 
-        Check if the return value is -1.
+        Check if the return values are -1 and log_e(1e-10).
         """
-        pass
+        value = np.exp(-1)
+        should_be_minus_one = CalculationUtils.safe_log_e(value=value)
+        assert should_be_minus_one == -1
 
-    @pytest.mark.utils
-    def test_safe_log_e_with_zero(self):
-        """Normal test;
-        run safe_log_e with 0.
-
-        Check if the return value is -16.
-        """
-        pass
+        value = 0
+        result = CalculationUtils.safe_log_e(value=value)
+        assert result == np.log(1e-16)
 
     @pytest.mark.utils
     def test_calc_2d_output_shape(self):
