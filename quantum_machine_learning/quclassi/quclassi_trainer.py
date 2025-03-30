a2c49d33af0adbde91000471560652bd3a0f0eeb
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2025-03-29 22:24:47 -0400

    #32 Move methods to calculate things into CalculationUtils

--- quantum_machine_learning/quclassi/quclassi_trainer.py
+++ quantum_machine_learning/quclassi/quclassi_trainer.py
@@ -10,6 +10,7 @@ import yaml
 from quantum_machine_learning.quclassi.quclassi import QuClassi
 from quantum_machine_learning.postprocessor.postprocessor import Postprocessor
 from quantum_machine_learning.utils.utils import Utils
+from quantum_machine_learning.utils.calculation_utils import CalculationUtils
 
 
 class QuClassiTrainer:
@@ -216,7 +217,7 @@ class QuClassiTrainer:
             for datum in data
         ]
         # Calculate the loss value.
-        loss = QuClassiTrainer.calculate_cross_entropy(
+        loss = CalculationUtils.calculate_cross_entropy(
             probabilities_list=probabilities_list, true_labels=[label] * len(data)
         )
         # Calculate the accuracy.
@@ -224,7 +225,7 @@ class QuClassiTrainer:
             max(probabilities, key=probabilities.get)
             for probabilities in probabilities_list
         ]
-        accuracy = QuClassiTrainer.calculate_accuracy(
+        accuracy = CalculationUtils.calculate_accuracy(
             predicted_labels=predicted_labels, true_labels=[label] * len(data)
         )
 
@@ -301,35 +302,3 @@ class QuClassiTrainer:
             )
 
         return fidelities
-
-    @staticmethod
-    def calculate_cross_entropy(
-        probabilities_list: list[dict[str, float]],
-        true_labels: list[str],
-    ):
-        if len(probabilities_list) != len(true_labels):
-            error_msg = f"The lengths of probabilities_list and true_labels must be same, but {len(probabilities_list)} vs {len(true_labels)}."
-            raise ValueError(error_msg)
-
-        cross_entropy = 0
-        for true_label, probabilities in zip(true_labels, probabilities_list):
-            predicted_label = max(probabilities, key=probabilities.get)
-            if true_label == predicted_label:
-                cross_entropy += -np.log(probabilities[predicted_label])
-            else:
-                cross_entropy += -np.log(1 - probabilities[predicted_label])
-
-        cross_entropy /= len(true_labels)
-
-        return cross_entropy
-
-    @staticmethod
-    def calculate_accuracy(
-        predicted_labels: list[str], true_labels: list[str]
-    ) -> float:
-        if len(predicted_labels) != len(true_labels):
-            error_mas = f"The lengths of the predicted and true labels must be the same, but {len(predicted_labels)} and {len(true_labels)}."
-            raise ValueError(error_mas)
-
-        num_correct = int((np.array(predicted_labels) == np.array(true_labels)).sum())
-        return num_correct / len(predicted_labels)
---
+++ quantum_machine_learning/utils/calculation_utils.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+class CalculationUtils:
+    """Utils, for calculation, class"""
+
+    @staticmethod
+    def calculate_cross_entropy(
+        probabilities_list: list[dict[str, float]],
+        true_labels: list[str],
+    ):
+        if len(probabilities_list) != len(true_labels):
+            error_msg = f"The lengths of probabilities_list and true_labels must be same, but {len(probabilities_list)} vs {len(true_labels)}."
+            raise ValueError(error_msg)
+
+        cross_entropy = 0
+        for true_label, probabilities in zip(true_labels, probabilities_list):
+            predicted_label = max(probabilities, key=probabilities.get)
+            if true_label == predicted_label:
+                cross_entropy += -np.log(probabilities[predicted_label])
+            else:
+                cross_entropy += -np.log(1 - probabilities[predicted_label])
+
+        cross_entropy /= len(true_labels)
+
+        return cross_entropy
+
+    @staticmethod
+    def calculate_accuracy(
+        predicted_labels: list[str], true_labels: list[str]
+    ) -> float:
+        if len(predicted_labels) != len(true_labels):
+            error_mas = f"The lengths of the predicted and true labels must be the same, but {len(predicted_labels)} and {len(true_labels)}."
+            raise ValueError(error_mas)
+
+        num_correct = int((np.array(predicted_labels) == np.array(true_labels)).sum())
+        return num_correct / len(predicted_labels)
