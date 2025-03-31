d74edb55d50bbcb94eb24a6fce97c1c17535944c
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2025-03-30 20:53:16 -0400

    Update BaseEncoder

--- quantum_machine_learning/encoders/base_encoder.py
+++ quantum_machine_learning/encoders/base_encoder.py
@@ -12,28 +12,29 @@ class BaseEncoder(qiskit.circuit.library.BlueprintCircuit, ABC):
         *regs: qiskit.QuantumCircuit,
         data_dimension: int,
         name: str | None = None,
-        transformer: Callable[[list[float]], list[float]] | None = None
     ):
         """Create a new encoder.
 
         :param int data_dimension: the dimension of data
         :param str | None name: the name of this encoder, defaults to None
-        :param Callable[[list[float]], list[float]] | None transformer: the data transformer, defaults to None
         """
         super().__init__(*regs, name=name)
 
         self._parameters: list[qiskit.circuit.ParameterVector] | None = None
-        self._transformer: Callable[[list[float]], list[float]] | None = transformer
         self._data_dimension: int | None = None
+
         self.data_dimension = data_dimension
 
     @property
-    def data_dimension(self) -> int | None:
+    def data_dimension(self) -> int:
         """Return the dimension of data.
 
-        :return int | None: the dimension of data
+        :return int: the dimension of data
         """
-        return self._data_dimension
+        if self._data_dimension is None:
+            return 0
+        else:
+            return self._data_dimension
 
     @data_dimension.setter
     def data_dimension(self, data_dimension: int) -> None:
@@ -44,24 +45,6 @@ class BaseEncoder(qiskit.circuit.library.BlueprintCircuit, ABC):
         self._data_dimension = data_dimension
         self._reset_register()
         self._reset_parameters()
-
-    @property
-    def transformer(self) -> Callable[[list[float]], list[float]] | None:
-        """Return the transformer.
-
-        :return Callable[[list[float]], list[float]] | None: transformer
-        """
-        return self._transformer
-
-    @transformer.setter
-    def transformer(
-        self, transformer: Callable[[list[float]], list[float]] | None
-    ) -> None:
-        """Set the transfromer.
-
-        :param Callable[[list[float]], list[float]] | None transformer: transformer
-        """
-        self._transformer = transformer
 
     @property
     @abstractmethod
@@ -94,16 +77,29 @@ class BaseEncoder(qiskit.circuit.library.BlueprintCircuit, ABC):
             )
             return num_parameters
 
-    def transform(self, data: list[float]) -> list[float]:
-        """Transform the given data using self.__transformer.
+    def _check_configuration(self, raise_on_failure: bool = True) -> bool:
+        """Check if the current configuration is valid.
 
-        :param list[float] data: data to be transformed
-        :return list[float]: transformed data
+        :param bool raise_on_failure: if raise an error or not, defaults to True
+        :raises AttributeError: if its data_dimension is non-positive.
+        :raises AttributeError: if its num_parameters is zero.
+        :return bool: if the configuration is valid
         """
-        if self._transformer is None:
-            return data
-        else:
-            return self._transformer(data)
+        valid = True
+
+        if self.data_dimension <= 0:
+            valid = False
+            if raise_on_failure:
+                error_msg = f"The data_dimension must be positive integer, but {self.data_dimension}."
+                raise AttributeError(error_msg)
+
+        if self.num_parameters == 0:
+            valid = False
+            if raise_on_failure:
+                error_msg = f"The number of parameters must be positive integer, but {self.num_parameters}."
+                raise AttributeError(error_msg)
+
+        return valid
 
     @abstractmethod
     def _reset_register(self) -> None:
