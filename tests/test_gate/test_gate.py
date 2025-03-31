79a9354523e8c866989e115ca1ae03b070c41617
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2025-03-31 13:14:13 -0400

    #36 Update the tests for SSwapGate

--- tests/test_gate/test_gate.py
+++ tests/test_gate/test_gate.py
@@ -12,17 +12,32 @@ class TestGate:
         pass
 
     @pytest.mark.gate
+    def test_init_with_defaults(self):
+        """Normal test;
+        create an instance of the square root swap gate.
+
+        Check if
+        1. its name is "√SWAP".
+        2. the number of qubits to be applied is two.
+        """
+        s_swap_gate = SSwapGate()
+        # 1. its name is "√SWAP".
+        assert s_swap_gate.name == "√SWAP"
+        # 2. the number of qubits to be applied is two.
+        assert s_swap_gate.num_qubits == 2
+
+    @pytest.mark.gate
     def test_sswap_matrix_representation(self):
         """Normal test;
+        create an instance of the square root swap gate.
+
         Check if
-        - the type of the instance of SSWapGate is qiskit.circuit.Gate.
-        - the matrix representation of SSwapGate is correct.
+        1. the matrix representation of SSwapGate is correct.
         """
         circuit = qiskit.QuantumCircuit(2)
         s_swap_gate = SSwapGate()
-        assert isinstance(s_swap_gate, qiskit.circuit.Gate)
+        # 1. the matrix representation of SSwapGate is correct.
         circuit.append(s_swap_gate, [0, 1])
-
         unitary = qiskit.quantum_info.Operator(circuit)
         correct_matrix = np.array(
             [
