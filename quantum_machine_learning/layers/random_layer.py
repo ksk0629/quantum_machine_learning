b94f0a8c40dcf7f2db674afb0745c294c2a925ca
ksk0629 <neko.to.tomodachi.muse@gmail.com>
2025-04-08 14:47:54 -0400

    #37 Update RandomLayer

--- quantum_machine_learning/layers/random_layer.py
+++ quantum_machine_learning/layers/random_layer.py
@@ -1,6 +1,7 @@
 import dataclasses
 import itertools
 import random
+from typing import Callable, Final
 
 import numpy as np
 import qiskit
@@ -43,11 +44,32 @@ class RandomLayer(BaseLayer):
     in https://arxiv.org/pdf/1904.04767.
     """
 
+    DEFAULT_AVAILABEL_GATES: Final[list[qiskit.circuit.Gate]] = [
+        GateInfo(qiskit.circuit.library.RXGate, 1, 1),
+        GateInfo(qiskit.circuit.library.RYGate, 1, 1),
+        GateInfo(qiskit.circuit.library.RZGate, 1, 1),
+        GateInfo(qiskit.circuit.library.PhaseGate, 1, 1),
+        GateInfo(qiskit.circuit.library.UGate, 1, 3),
+        GateInfo(qiskit.circuit.library.TGate, 1, 0),
+        GateInfo(qiskit.circuit.library.HGate, 1, 0),
+        GateInfo(qiskit.circuit.library.CUGate, 2, 4),
+        GateInfo(SSwapGate, 2, 0),
+        GateInfo(qiskit.circuit.library.CXGate, 2, 0),
+        GateInfo(qiskit.circuit.library.SwapGate, 2, 0),
+    ]
+    DEFAULT_THRESHOLD: Final[float] = 0.5
+    DEFAULT_SELECT_OPTIONS: Final[Callable[[int], SelectOptions]] = (
+        lambda num_state_qubits: SelectOptions(
+            {1: 2 * num_state_qubits**2, 2: 1},  # a maximum number of gates
+            {1: 0, 2: 1},  # a minimum number of gates
+        )
+    )
+
     def __init__(
         self,
         num_state_qubits: int,
         available_gates: list[GateInfo] | None = None,
-        threshold: float = 0.5,
+        threshold: float | None = None,
         select_options: SelectOptions | None = None,
         seed: int = 901,
         name: str | None = "RandomLayer",
@@ -55,10 +77,11 @@ class RandomLayer(BaseLayer):
         """Initialise this layer.
 
         :param int num_state_qubits: the number of state qubits
-        :param list[GateInfo] | None available_gates: a set of GateInfo
-        :param float threshold: a threshold of connection probability
-        :param SelectOptions | None select_options: a select options for gates
-        :param str | None name: the name of this encoder, defaults to "RandomLayers"
+        :param list[GateInfo] | None available_gates: a set of gates, defaults to None
+        :param float | None threshold: a threshold of connection probability, defaults to None
+        :param SelectOptions | None select_options: a select options for gates, None
+        :param int | None seed: a random seed, defaults to 901
+        :param str | None name: the name of this encoder, defaults to "RandomLayer"
         """
         self._selected_gates: list[SelectedGate] | None = None
         self._connection_probabilities: dict[list[int], float] | None = None
@@ -75,7 +98,7 @@ class RandomLayer(BaseLayer):
         self.seed = seed
 
     @property
-    def available_gates(self) -> list[GateInfo] | None:
+    def available_gates(self) -> list[GateInfo]:
         """Return available gates.
         If it is None, then return the standard availalbe gates defined here.
 
@@ -84,43 +107,27 @@ class RandomLayer(BaseLayer):
         if self._available_gates is not None:
             return self._available_gates
         else:
-            return [
-                GateInfo(qiskit.circuit.library.RXGate, 1, 1),
-                GateInfo(qiskit.circuit.library.RYGate, 1, 1),
-                GateInfo(qiskit.circuit.library.RZGate, 1, 1),
-                GateInfo(qiskit.circuit.library.PhaseGate, 1, 1),
-                GateInfo(qiskit.circuit.library.UGate, 1, 3),
-                GateInfo(qiskit.circuit.library.TGate, 1, 0),
-                GateInfo(qiskit.circuit.library.HGate, 1, 0),
-                GateInfo(qiskit.circuit.library.CUGate, 2, 4),
-                GateInfo(SSwapGate, 2, 0),
-                GateInfo(qiskit.circuit.library.CXGate, 2, 0),
-                GateInfo(qiskit.circuit.library.SwapGate, 2, 0),
-            ]
+            return RandomLayer.DEFAULT_AVAILABEL_GATES
 
     @available_gates.setter
     def available_gates(self, available_gates: list[GateInfo] | None) -> None:
         """Set the new available gates if valid and reset the register.
 
         :param list[GateInfo] available_gates: a new available gates
-        :raises ValueError: if the given available gates contain non-GateInfo
         """
-        if available_gates is not None:
-            for availabel_gate in available_gates:
-                if not isinstance(availabel_gate, GateInfo):
-                    error_msg = f"available_gates must be a list of GateInfo, but it includes non-GateInfo: {available_gates}"
-                    raise ValueError(error_msg)
-
         self._available_gates = available_gates
         self._reset_register()
 
     @property
-    def threshold(self) -> float | None:
+    def threshold(self) -> float:
         """Return the threshold of connection probability.
 
-        :return float | None: threshold
+        :return float: the threshold
         """
-        return self._threshold
+        if self._threshold is None:
+            return RandomLayer.DEFAULT_THRESHOLD
+        else:
+            return self._threshold
 
     @threshold.setter
     def threshold(self, threshold: float | None) -> None:
@@ -154,10 +161,8 @@ class RandomLayer(BaseLayer):
         if self._select_options is not None:
             return self._select_options
         else:
-            max_num_gates = {1: 2 * self.num_state_qubits**2, 2: 1}
-            min_num_gate = {1: 0, 2: 1}
 
-            return SelectOptions(max_num_gates, min_num_gate)
+            return RandomLayer.DEFAULT_SELECT_OPTIONS(self.num_state_qubits)
 
     @select_options.setter
     def select_options(self, select_options: SelectOptions | None) -> None:
@@ -169,12 +174,15 @@ class RandomLayer(BaseLayer):
         self._reset_register()
 
     @property
-    def connection_probabilities(self) -> dict[list[int], float] | None:
+    def connection_probabilities(self) -> dict[list[int], float]:
         """Return the connection probabilities between qubits.
 
-        :return dict[list[int], float] | None: the connection probabilities
+        :return dict[list[int], float]: the connection probabilities
         """
-        return self._connection_probabilities
+        if self._connection_probabilities is None:
+            return dict()
+        else:
+            return self._connection_probabilities
 
     def _set_connection_probabilieis(self):
         """Randomly assign connection probabilities between each qubit.
@@ -192,12 +200,15 @@ class RandomLayer(BaseLayer):
         self._connection_probabilities = connection_probabilities
 
     @property
-    def selected_gates(self) -> list[SelectedGate] | None:
+    def selected_gates(self) -> list[SelectedGate]:
         """Return the selected gates.
 
-        :return list[SelectedGate] | None: the selected gate
+        :return list[SelectedGate]: the selected gate
         """
-        return self._selected_gates
+        if self._selected_gates is None:
+            return []
+        else:
+            return self._selected_gates
 
     def _set_gates_based_on_probabilities(self, target_num_qubits: int) -> None:
         """Select gates based on probabilities based on the connection probabilities.
@@ -251,9 +262,33 @@ class RandomLayer(BaseLayer):
         """Check if the current configuration is valid.
 
         :param bool raise_on_failure: if raise an error or not, defaults to True
+        :raises AttributeError: if there is a gate requiring a larget number qubits than its num_state_qubits
+        :raises AttributeError: if its select_options is invalid.
         :return bool: if the configuration is valid
         """
-        valid = True
+
+        valid = super()._check_configuration(raise_on_failure=raise_on_failure)
+        if not all(
+            1 <= available_gate.num_qubits <= self.num_state_qubits
+            for available_gate in self.available_gates
+        ):
+            valid = False
+            if raise_on_failure:
+                error_msg = f"available_gates must be a set of gates being able to apply to this circuit having {self.num_state_qubits} qubits. However, some gates require more qubits."
+                raise AttributeError(error_msg)
+
+        num_qubits_for_max = set(list(self.select_options.max_num_gates.keys()))
+        num_qubits_for_min = set(list(self.select_options.min_num_gates.keys()))
+        num_qubits_list = list(num_qubits_for_max & num_qubits_for_min)  # intersection
+        for num_qubits in num_qubits_list:
+            max_num = self.select_options.max_num_gates[num_qubits]
+            min_num = self.select_options.min_num_gates[num_qubits]
+            if max_num < min_num:
+                valid = False
+                if raise_on_failure:
+                    error_msg = f"For {num_qubits} qubits, the maximum number of gates vs the minimum number of gates = {max_num} vs {min_num}, which is invalid."
+                    raise AttributeError(error_msg)
+
         return valid
 
     def _reset_register(self) -> None:
@@ -266,7 +301,8 @@ class RandomLayer(BaseLayer):
         super()._build()
 
         # Fix the seed.
-        Utils.fix_seed(self.seed)
+        if self.seed is not None:
+            Utils.fix_seed(self.seed)
 
         # Set connection probabilities between qubits.
         self._set_connection_probabilieis()
@@ -274,16 +310,19 @@ class RandomLayer(BaseLayer):
         # Get gates randomly in available gates.
         self._selected_gates = []
         for n in range(1, self.num_state_qubits + 1):  # type: ignore
+            # Get a maximum number of n-qubit gates.
             if n in self.select_options.max_num_gates:
                 max_num = self.select_options.max_num_gates[n]
             else:
                 max_num = 1
 
+            # Get a minimum number of n-qubit gates.
             if n in self.select_options.min_num_gates:
                 min_num = self.select_options.min_num_gates[n]
             else:
                 min_num = 1
 
+            # Determine the number of n-qubit gates.
             if max_num == min_num:
                 num_gates = max_num
             else:
